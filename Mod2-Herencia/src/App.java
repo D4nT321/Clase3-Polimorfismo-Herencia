10927af770255d752acc82ec6f0cfb5f1d687ca9
D4nT321 <dantedemonio73@gmail.com>
2025-11-27 11:20:09 -0500

    inherence example (Herencia)

--- Mod2-Herencia/src/App.java
+++ Mod2-Herencia/src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        animalSample();
+    }
+
+    private static void animalSample() {
+        Dog animal = new Dog("Firulais", 3, "Labrador");
+        
+
+        System.out.println("Nombre: " + animal.getName());
+        animal.makeNoise();
+        animal.eat("croquetas");
+        animal.bark();
+        animal.sleep();
+        
+        Cat animal2 = new Cat("Dante", 4);
+        System.out.println("Nombre: " + animal2.getName());
+        animal2.makeNoise();
+        animal2.eat("Ratones");
+        animal2.scratch();
+        animal2.sleep();
     }
 }
---
+++ Mod2-Herencia/src/Cat.java
@@ -0,0 +1,21 @@
+public class Cat extends Animal {
+
+    public Cat(String name, Integer age) {
+        super(name, age);
+    }
+
+    public void eat(String food) {
+        System.out.println(getName() + " esta comiendo " + food);
+    }
+
+    public void scratch() {
+        System.out.println(" está rascando.");
+
+    }
+    @Override
+    public void makeNoise() {
+        System.out.println("Miau Miau");
+    }
+
+    
+}
